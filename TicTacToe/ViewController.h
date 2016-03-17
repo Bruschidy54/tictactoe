94568cc3cc330b9ffad717611545f13f8e2713d9
Dylan Bruschi <dylanbruschi54@gmail.com>
2016-03-17 13:50:34 -0500

    Initial Commit

---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Dylan Bruschi on 3/17/16.
+//  Copyright © 2016 Dylan Bruschi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TicTacToe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TicTacToe
+//
+//  Created by Dylan Bruschi on 3/17/16.
+//  Copyright © 2016 Dylan Bruschi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
